d4150432fe0dc8dfeea9044d0a5ff9290f83465d
sutne <sivertutne@gmail.com>
2023-11-19 15:04:01 +0100

    feat: add firstEarnedTrophy DateTime for games

--- api/playstation/trophies.ts
+++ api/playstation/trophies.ts
@@ -2,8 +2,9 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { getGames } from '../../src/playstation/util/api/game-list';
 import { getTrophyGroups } from '../../src/playstation/util/api/trophy-list';
-import { TrophyGame } from '../../src/playstation/types';
+import { Trophy, TrophyGame } from '../../src/playstation/types';
 import { cacheControl, compareDate } from '../../src/util';
+import { TrophyTitle } from 'psn-api';
 
 /**
  * @returns either trophy overview for a game, or all trophies associated with
@@ -23,10 +24,40 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   } else {
     // TrophyGame[]
     // returns all games with their trophy overview
-    const games = await getGames();
-    const sorted = games.sort((a, b) =>
-      compareDate(a.lastUpdatedDateTime, b.lastUpdatedDateTime),
+    const games = (await getGames()) as (TrophyTitle & {
+      firstUpdatedDateTime?: string;
+    })[];
+
+    // Find earliest trophy earned for each game
+    await Promise.all(
+      games.map(async (game) => {
+        const groups = await getTrophyGroups(
+          game.npCommunicationId,
+          game.trophyTitlePlatform,
+        );
+        // gather all group trophies ot single list
+        const trophies = groups.reduce(
+          (trophies, group) => trophies.concat(group.trophies),
+          [] as Trophy[],
+        );
+
+        const earliestTrophy = trophies.reduce((earliest, trophy) => {
+          if (!trophy.isEarned) return earliest;
+          if (!earliest) return trophy;
+          const newIsEarlier =
+            compareDate(earliest.earnedAt, trophy.earnedAt) < 0;
+          return newIsEarlier ? trophy : earliest;
+        }, null as Trophy | null);
+
+        if (earliestTrophy !== null) {
+          game.firstUpdatedDateTime = earliestTrophy.earnedAt ?? '';
+        }
+      }),
     );
+
+    const sorted = games.sort((a, b) => {
+      return compareDate(a.lastUpdatedDateTime, b.lastUpdatedDateTime);
+    });
     response = sorted.map((game) => {
       const trophyGame: TrophyGame = {
         id: game.npCommunicationId,
@@ -36,6 +67,8 @@ export default async function (req: VercelRequest, res: VercelResponse) {
         trophyCount: game.definedTrophies,
         earnedCount: game.earnedTrophies,
         progress: game.progress,
+        firstTrophyEarnedAt: game.firstUpdatedDateTime,
+        lastTrophyEarnedAt: game.lastUpdatedDateTime,
       };
       return trophyGame;
     });
--- src/playstation/types.ts
+++ src/playstation/types.ts
@@ -33,6 +33,8 @@ export type TrophyGame = {
   trophyCount: TrophyCount;
   earnedCount: TrophyCount;
   progress: number;
+  firstTrophyEarnedAt?: string;
+  lastTrophyEarnedAt?: string;
 };
 
 export type TrophyGroup = {

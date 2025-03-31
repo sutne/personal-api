126a5f3851d73bc7283a41d42173c23b45cce8e8
sutne <sivert.utne@gmail.com>
2025-03-31 17:53:54 +0200

    fix: add more details to all-trophies trophies

--- src/playstation/get-all-trophies.ts
+++ src/playstation/get-all-trophies.ts
@@ -1,21 +1,40 @@
 import type { TrophyTitle } from 'psn-api';
 import { compareDate } from '../util';
-import type { Trophy } from './types';
+import type { CompleteTrophy } from './types';
 import { getGames } from './util/api/game-list';
 import { getTrophyGroups } from './util/api/trophy-list';
 import { platform } from './util/platforms';
 
-/** returns all trophies across all games as Trophy[], wether they are earned or not */
-export async function getAllTrophies(): Promise<Trophy[]> {
+/** returns all trophies across all games as CompleteTrophy[], wether they are earned or not */
+export async function getAllTrophies(): Promise<CompleteTrophy[]> {
   const trophyTitles: TrophyTitle[] = await getGames();
-  const allGameTrophies: Trophy[][] = await Promise.all(
+  const allGameTrophies: CompleteTrophy[][] = await Promise.all(
     trophyTitles.map(async (trophyTitle) => {
       const groups = await getTrophyGroups(
         trophyTitle.npCommunicationId,
         platform(trophyTitle.trophyTitlePlatform),
       );
-      const trophies: Trophy[] = groups.reduce(
-        (trophies, group) => trophies.concat(group.trophies),
+      const trophies: CompleteTrophy[] = groups.reduce(
+        (trophies, group) =>
+          trophies.concat(
+            group.trophies.map((trophy) => {
+              const complete: CompleteTrophy = {
+                ...trophy,
+                group: {
+                  id: group.id,
+                  name: group.name,
+                  icon: group.icon,
+                },
+                game: {
+                  id: trophyTitle.npCommunicationId,
+                  name: trophyTitle.trophyTitleName,
+                  icon: trophyTitle.trophyTitleIconUrl,
+                  platform: platform(trophyTitle.trophyTitlePlatform),
+                },
+              };
+              return complete;
+            }),
+          ),
         [],
       );
       return trophies;

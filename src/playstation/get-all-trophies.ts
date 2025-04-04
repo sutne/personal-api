81be6e888e05d459a71dd87c0474e11289358d4d
sutne <sivert.utne@gmail.com>
2025-04-05 00:14:52 +0200

    fix: more nuances sorting for all trophies

--- src/playstation/get-all-trophies.ts
+++ src/playstation/get-all-trophies.ts
@@ -1,6 +1,6 @@
 import type { TrophyTitle } from 'psn-api';
 import { compareDate } from '../util';
-import type { CompleteTrophy } from './types';
+import type { CompleteTrophy, Trophy } from './types';
 import { getGames } from './util/api/game-list';
 import { getTrophyGroups } from './util/api/trophy-list';
 import { platform } from './util/platforms';
@@ -41,9 +41,48 @@ export async function getAllTrophies(): Promise<CompleteTrophy[]> {
     }),
   );
   const allTrophies = allGameTrophies.flat().sort((a, b) => {
-    const earnedDiff = compareDate(a.earnedAt, b.earnedAt);
-    if (earnedDiff !== 0) return earnedDiff;
-    return Number(b.rarity) - Number(a.rarity);
+    if (a.isEarned && b.isEarned) return compareEarnedTrophy(a, b);
+    if (!a.isEarned && !b.isEarned) return compareUnearnedTrophy(a, b);
+    return Number(b.isEarned) - Number(a.isEarned);
   });
   return allTrophies;
+}
+
+function compareEarnedTrophy(a: CompleteTrophy, b: CompleteTrophy) {
+  if (!a.earnedAt && !b.earnedAt) {
+    // both trophies are missing timestamps, so no way to compare them that way
+    const gameDiff = a.game.name.localeCompare(b.game.name);
+    if (gameDiff) return gameDiff;
+    return a.id - b.id; // id in reverse order (first id comes last, assumed to have been achieved first)
+  }
+  const earnedDiff = compareDate(a.earnedAt, b.earnedAt);
+  if (earnedDiff !== 0) return earnedDiff;
+  const rarityDiff = Number(a.rarity) - Number(b.rarity);
+  if (Math.abs(rarityDiff) > 0.09) return rarityDiff;
+  return compareTrophyType(a, b);
+}
+
+function compareUnearnedTrophy(a: CompleteTrophy, b: CompleteTrophy) {
+  const progressDiff =
+    (a.progress?.achieved ?? 0 / a.progress?.target ?? 1) -
+    (b.progress?.achieved ?? 0 / b.progress?.target ?? 1);
+  if (Math.abs(progressDiff) > 0.0001) return progressDiff;
+  const rarityDiff = Number(b.rarity) - Number(a.rarity);
+  if (Math.abs(rarityDiff) > 0.09) return rarityDiff;
+  return compareTrophyType(b, a);
+}
+
+function compareTrophyType(a: Trophy, b: Trophy) {
+  const typeValues = {
+    platinum: 0,
+    gold: 1,
+    silver: 2,
+    bronze: 3,
+  };
+  const hideA = a.isHidden && !a.isEarned;
+  const hideB = b.isHidden && !b.isEarned;
+  if (hideA && hideB) return 0;
+  if (hideA) return 1;
+  if (hideB) return -1;
+  return typeValues[a.type] - typeValues[b.type];
 }

dd01b3f94e87c205eb9dc7d6ebf866ac3a08dd0a
sutne <sivert.utne@gmail.com>
2025-04-06 15:35:59 +0200

    feat: add some custom ignore rules

--- src/playstation/get-all-trophies.ts
+++ src/playstation/get-all-trophies.ts
@@ -1,5 +1,5 @@
 import type { TrophyTitle } from 'psn-api';
-import type { CompleteTrophy } from './types';
+import type { CompleteTrophy, Platform } from './types';
 import { getGames } from './util/api/game-list';
 import { getTrophyGroups } from './util/api/trophy-list';
 import { platform } from './util/platforms';
@@ -13,6 +13,23 @@ import {
   compareTrophyRarity,
   compareTrophyType,
 } from './util/trophy-sorting';
+
+type IgnoreRule = {
+  name: string;
+  platforms: Platform[];
+};
+/**
+ * manual list of games where all my remaining trophies are
+ * unobtainable (servers are no longer online, game is no longer playable),
+ * or if i for other reasons am never going to be getting those trophies.
+ */
+const IGNORED_UNEARNED_IN_GAMES: IgnoreRule[] = [
+  { name: 'Burnout™ Paradise', platforms: ['PS3'] }, // online servers shut down, not getting remaining single player ones
+  { name: 'Battlefield: Bad Company™ 2', platforms: ['PS3'] }, // online servers shut down, DLC no longer purchasable
+  { name: 'Need for Speed™ Most Wanted', platforms: ['PS3'] }, // online servers shut down
+  { name: 'Grand Theft Auto V', platforms: ['PS3'] }, // online servers shut down
+  { name: 'Fall Guys', platforms: ['PS4'] }, // game is no longer playable (replaced when bought by Epic)
+];
 
 /** returns all trophies across all games as CompleteTrophy[], wether they are earned or not */
 export async function getAllTrophies(): Promise<CompleteTrophy[]> {
@@ -49,11 +66,23 @@ export async function getAllTrophies(): Promise<CompleteTrophy[]> {
       return trophies;
     }),
   );
-  const allTrophies = allGameTrophies.flat().sort((a, b) => {
-    if (a.isEarned && b.isEarned) return compareEarnedTrophies(a, b);
-    if (!a.isEarned && !b.isEarned) return compareUnearnedTrophies(a, b);
-    return Number(b.isEarned) - Number(a.isEarned); // earned trophies first
-  });
+  const allTrophies = allGameTrophies
+    .flat()
+    .filter((trophy) => {
+      if (!trophy.isEarned) {
+        for (const rule of IGNORED_UNEARNED_IN_GAMES) {
+          if (trophy.game.name === rule.name) {
+            return !rule.platforms.includes(trophy.game.platform);
+          }
+        }
+      }
+      return true;
+    })
+    .sort((a, b) => {
+      if (a.isEarned && b.isEarned) return compareEarnedTrophies(a, b);
+      if (!a.isEarned && !b.isEarned) return compareUnearnedTrophies(a, b);
+      return Number(b.isEarned) - Number(a.isEarned); // earned trophies first
+    });
   return allTrophies;
 }
 
